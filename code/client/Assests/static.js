592e6bce15ef02cccafacc737e6c2b421254c328
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2022-10-04 13:59:48 +0530

    widgets ui fixed

--- code/client/Assests/static.js
+++ code/client/Assests/static.js
@@ -1,14 +1,14 @@
 export const news = [
   {
-    category: "Clever Programmer • LIVE",
-    title: "CP is live on YouTube!",
+    category: "Blog-X is Live!",
+    title: "Security at its fullest!",
     image:
       "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
   },
   {
     category: "Databases • Trending",
     title:
-      "Sanity is giving you free Boosted plan, use --coupon cleverprogrammer",
+      "Sanity is giving you free Boosted plan, use --coupon code: 'blog-x' to get 50% off",
     image: "https://avatars.githubusercontent.com/u/17177659?s=280&v=4",
   },
   {
@@ -27,14 +27,14 @@ export const news = [
 
 export const whoToFollow = [
   {
-    name: "Rafeh Qazi",
-    handle: "@cleverqazi",
+    name: "Test Link1",
+    handle: "@tl1",
     avatar:
       "https://pbs.twimg.com/profile_images/1472075657920221184/JhAI3WGm_400x400.jpg",
   },
   {
-    name: "David Rakosi",
-    handle: "@0xDavidRakosi",
+    name: "Test link2",
+    handle: "@tl2",
     avatar:
       "https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg",
   },
