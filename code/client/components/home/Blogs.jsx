2bb8752fb797a8192a930b78481d2de6e1b4bd0c
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2022-10-25 16:26:47 +0530

    [feat] : blogs visible on home and profile page

--- code/client/components/home/Blogs.jsx
+++ code/client/components/home/Blogs.jsx
@@ -15,11 +15,11 @@ const style = {
 
 
 const Blogs = ({ profile }) => {
-    const [tweetArr, setTweetArr] = useState();
+    const [blogArr, setblogArr] = useState();
     const { Moralis, account } = useMoralis();
 
     useEffect(() => {
-        async function getTweets() {
+        async function getblogs() {
             try {
                 const Blogs = Moralis.Object.extend("Blogs");
                 const query = new Moralis.Query(Blogs);
@@ -28,62 +28,60 @@ const Blogs = ({ profile }) => {
                 }
                 const results = await query.find();
 
-                setTweetArr(results);
+                setblogArr(results);
                 console.log(results);
             } catch (error) {
                 console.error(error);
             }
         }
-        getTweets();
+        getblogs();
     }, [profile]);
 
     return (
-        <div className='outchecker'>
-            {tweetArr?.map((e) => {
-                <div className='checker'>
-                    <div className='px-4 py-2'>
-                        <div className={style.blogs}>
-                            <div className={style.profile}>
-                                <div className={style.profilechars}>{e.attributes.UserName}</div>
-                                <div className={style.profilechars}>{e.attributes.UserAccount}</div>
-                                <Image
-                                    src="/favicon.ico"
-                                    alt="Picture of the author"
-                                    width={30}
-                                    height={30}
-                                />
+        <>
+            {blogArr && blogArr.map((blog) => (
+                <div className={style.blogs}>   
+                    <div className={style.profile}>
+                        <div className="flex-shrink-0">
+                            <Image
+                                className="rounded-full"
+                                src={blog.attributes.UserImage}
+                                alt=""
+                                width={40}
+                                height={40}
+                            />
+                        </div>
+                        <div className={style.profilechars}>
+                            <p className="font-bold">{blog.attributes.UserName}</p>
+                            <p className="text-gray-500">{blog.attributes.UserAccount.slice(0, 10) + "...." + blog.attributes.UserAccount.slice(-4)}</p>
+                        </div>
+                    </div>
+                    <div className="text-md text-gray-500 pt-2">
+                        {blog.attributes.blogTxt}
+                    </div>
+                    <div className={style.engage}>
+                        <div className="flex flex-row gap-2">
+                            <div className="flex items-center gap-1">
+                                <Star className="w-5 h-5 text-yellow-400" />
+                                <p>{blog.attributes.Likes}</p>
                             </div>
-                            <div className={style.content}>
-                                
-                                    hi i am a blog
-                                    {
-                                        console.log(e.attributes.blogTxt)
-                                    }
-                                    {e.attributes.blogTxt}
-                                
+                            <div className="flex items-center gap-1">
+                                <MessageCircle className="w-5 h-5 text-blue-500" />
+                                <p>{blog.attributes.Comments}</p>
                             </div>
-                            <div className={style.engage}>
-                                <div className="star hover:bg-gray-300 p-1 rounded-2xl">
-                                    <Star />
-                                </div>
-                                <div className="comment hover:bg-gray-300 p-1 rounded-2xl">
-                                    <MessageCircle />
-                                </div>
-                                <div className="share hover:bg-gray-300 p-1 rounded-2xl">
-                                    <BiTransfer />
-                                </div>
-                                <div className="matic hover:bg-gray-300 p-1 rounded-2xl">
-                                    <Matic />
-                                </div>
+                            <div className="flex items-center gap-1">
+                                <BiTransfer className="w-5 h-5 text-green-400" />
+                                <p>{blog.attributes.Shares}</p>
                             </div>
-
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <Matic className="w-5 h-5 text-matic-500" />
+                            <p>{blog.attributes.Matic}</p>
                         </div>
                     </div>
                 </div>
-            }).reverse()}
-        </div>
+            )).reverse()}
+        </>
     )
 }
-
-
 export default Blogs
