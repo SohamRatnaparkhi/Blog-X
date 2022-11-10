00e5c0b7ec03b4103cf70a28edb8e89a6ffc6dab
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2022-11-10 15:42:41 +0530

    login UI upgraded

--- code/client/components/LoginComp.jsx
+++ code/client/components/LoginComp.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { useMoralis } from 'react-moralis';
 // import { ConnectButton, Icon } from 'web3uikit';
+import Image from 'next/image';
 import { ConnectButton } from './home/ConnectButton';
 
 const LoginComp = () => {
   return (
     <div className='flex flex-col sm:flex-row'>
       <div className='basis-1/3 text-center'>
-        
+
       </div>
       <div className='basis-1/3 text-center mt-20'>
-        <div className='text-3xl font-bold hover:underline-offset-4 hover:text-4xl hover:pb-8 pb-5'>BlogX</div>
-        <ConnectButton />
+        <div className='text-3xl font-bold hover:underline-offset-4 hover:text-4xl hover:pb-8 pb-5 mt-20'>
+          <img src="/brandLogo_removebg2.png" className='mx-auto ' alt="Blog-X" srcset="" />
+        </div>
+        <div className=''> 
+          <ConnectButton />
+        </div>
       </div>
       <div className='basis-1/3 text-center'>
-        
+
       </div>
     </div>
   )
--- code/client/components/home/ConnectButton.jsx
+++ code/client/components/home/ConnectButton.jsx
@@ -44,7 +44,7 @@ export const ConnectButton = () => {
 
     return (
         <div className=''>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleAuth("metamask")}>Authenticate via metamask</button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleAuth("metamask")}>Login/Sign-up to Blog-X</button>
         </div>
     )
 }
