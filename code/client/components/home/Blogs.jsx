a3672a39c1a8148f5da11ecdbeae305df110271a
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-11-12 00:58:10 +0530

    fix: blog syntax errors

--- code/client/components/home/Blogs.jsx
+++ code/client/components/home/Blogs.jsx
@@ -1,105 +1,85 @@
-import React from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { Star, Copy, MessageCircle } from "@web3uikit/icons";
-import { BiTransfer } from "react-icons/bi";
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/router';
+import { Star, Copy, MessageCircle } from '@web3uikit/icons'
+import { BiTransfer } from 'react-icons/bi'
 import { useMoralis } from "react-moralis";
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import styles from "./Blogs.module.css";
+import Link from 'next/link'
+import styles from './Blogs.module.css'
 
 const style = {
-  blogs: `bg-[#fff] text-[#15202b] p-4 rounded-lg shadow-md text-left mt-4 flex flex-col`,
-  profile: `flex items-center flex-row p-2`,
-  profilechars: `flex-1 text-md font-bold`,
-  engage: `flex flex-row justify-between items-center pt-4 pb-1 hower:shadow-md`,
-  blogText: `text-md font-bold`,
-};
+    blogs: `bg-[#fff] text-[#15202b] p-4 rounded-lg shadow-md text-left mt-4 flex flex-col`,
+    profile: `flex items-center flex-row p-2`,
+    profilechars: `flex-1 text-md font-bold`,
+    engage: `flex flex-row justify-between items-center pt-4 pb-1 hower:shadow-md`,
+    blogText: `text-md font-bold`,
+}
 
 const Blogs = ({ profile }) => {
-  const [blogArr, setblogArr] = useState();
-  const {
-    Moralis,
-    account,
-    isAuthenticated,
-    isWeb3Enabled,
-    isWeb3EnableLoading,
-  } = useMoralis();
-  const currentUser = Moralis.User.current();
-  const router = useRouter();
-  const { asPath } = useRouter();
-  // console.log(account)
-  useEffect(() => {
-    async function getblogs() {
-      try {
+    const [blogArr, setblogArr] = useState();
+    const { Moralis, account, isAuthenticated, isWeb3Enabled, isWeb3EnableLoading } = useMoralis();
+    const currentUser = Moralis.User.current();
+    const router = useRouter();
+    const { asPath } = useRouter();
+    // console.log(account)
+    useEffect(() => {
+        async function getblogs() {
+            try {
+                const Blogs = Moralis.Object.extend("Blogs");
+                const query = new Moralis.Query(Blogs);
+                // console.log(profile)
+                if (profile == true) {
+                    // console.log(Blogs.attribute.UserAccount)
+                    console.log(account)
+                    query.equalTo("UserAccount", currentUser.attributes.ethAddress);
+                }
+                const results = await query.find();
+
+                setblogArr(results);
+                // console.log(results);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getblogs();
+    }, [profile]);
+
+    const refreshData = () => router.replace(router.asPath);
+
+    const incrementLikes = async (id) => {
         const Blogs = Moralis.Object.extend("Blogs");
         const query = new Moralis.Query(Blogs);
-        // console.log(profile)
-        if (profile == true) {
-          // console.log(Blogs.attribute.UserAccount)
-          console.log(account);
-          query.equalTo("UserAccount", currentUser.attributes.ethAddress);
-        }
-        const results = await query.find();
-
-        setblogArr(results);
-        // console.log(results);
-      } catch (error) {
-        console.error(error);
-      }
+        // console.log(id);
+        const blog = await query.get(id);
+        blog.increment("Likes");
+        await blog.save();
+        refreshData();
     }
-    getblogs();
-  }, [profile]);
-
-  const refreshData = () => router.replace(router.asPath);
-
-  const incrementLikes = async (id) => {
-    const Blogs = Moralis.Object.extend("Blogs");
-    const query = new Moralis.Query(Blogs);
-    // console.log(id);
-    const blog = await query.get(id);
-    blog.increment("Likes");
-    await blog.save();
-    refreshData();
-  };
-  return (
-    <>
-      {blogArr &&
-        blogArr
-          .map((blog) => (
-            <div key={blog._id} className={style.blogs}>
-              <div>
-                <div className={style.profile}>
-                  <div className="flex-shrink-0">
-                    <Image
-                      className="rounded-full"
-                      src={
-                        blog.attributes.UserImage
-                          ? blog.attributes.UserImage
-                          : "/pfp1.png"
-                      }
-                      alt=""
-                      width={40}
-                      height={40}
-                    />
-                  </div>
-                  <div className={style.profilechars}>
-                    <p className="font-bold">{blog.attributes.UserName}</p>
-                    <p className="text-gray-500">
-                      {blog.attributes.UserAccount.slice(0, 10) +
-                        "...." +
-                        blog.attributes.UserAccount.slice(-4)}
-                    </p>
-                  </div>
-                </div>
-                <div className={style.blogText}>
-                  <Link href={"/blog/" + blog.id} key={blog.id}>
-                    <div className={styles.blog_text}>
-                      {" "}
-                      {blog.attributes.blogTxt}{" "}
-                    </div>
-                  </Link>
-
+    return (
+        <>
+            {blogArr && blogArr.map((blog) => (
+                <div key={blog._id} className={style.blogs}>
+                    <div>
+                        <div className={style.profile}>
+                            <div className="flex-shrink-0">
+                                <Image
+                                    className="rounded-full"
+                                    src={blog.attributes.UserImage ? blog.attributes.UserImage : "/pfp1.png"}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                />
+                            </div>
+                            <div className={style.profilechars}>
+                                <p className="font-bold">{blog.attributes.UserName}</p>
+                                <p className="text-gray-500">{blog.attributes.UserAccount.slice(0, 10) + "...." + blog.attributes.UserAccount.slice(-4)}</p>
+                            </div>
+                        </div>
+                        <div className={style.blogText}>
+                            <Link href={'/blog/' + blog.id} key={blog.id} >
+                                <div className={styles.blog_text}> {blog.attributes.blogTxt} </div>
+                            </Link>
 
                             <img src={blog.attributes.blogImg} alt="" />
                         </div>
@@ -133,75 +113,38 @@ const Blogs = ({ profile }) => {
                         <div>
                             <p>{blog.attributes.createdAt.toDateString()}</p>
                         </div>
-
                     </div>
-                    <div className="flex items-center gap-1">
-                      <BiTransfer
-                        className="w-5 h-5 text-green-400"
-                        onClick={() => {
-                          navigator.clipboard.writeText(
-                            asPath + "blogs/" + blog.id
-                          );
-                          alert(
-                            "Copied to clipboard - " +
-                              asPath +
-                              "blogs/" +
-                              blog.id
-                          );
-                        }}
-                      />
-                      <p>{blog.attributes.Shares}</p>
-                    </div>
-                  </div>
-                  <div
-                    className="flex items-center gap-1"
-                    onClick={() => {
-                      navigator.clipboard.writeText(
-                        blog.attributes.UserAccount
-                      );
-                      alert("Copied to clipboard!");
-                    }}
-                  >
-                    <Copy />
-                  </div>
                 </div>
-                <div>
-                  <p>{blog.attributes.createdAt.toDateString()}</p>
-                </div>
-              </div>
-            </div>
-          ))
-          .reverse()}
-    </>
-  );
-};
+            )).reverse()
+            }
+        </>
+    )
+}
 
-export default Blogs;
+export default Blogs
 
 export async function getAllPostIds() {
-  // Instead of the file system,
-  // fetch post data from an external API endpoint
-  const res = await fetch(
-    "mongodb+srv://blogx-db:blogx@cluster0.c1ockvh.mongodb.net/parse?retryWrites=true&w=majority"
-  );
-  const posts = await res.json();
-  return posts.map((blog) => {
-    return {
-      params: {
-        id: blog.id,
-      },
-    };
-  });
+    // Instead of the file system,
+    // fetch post data from an external API endpoint
+    const res = await fetch('mongodb+srv://blogx-db:blogx@cluster0.c1ockvh.mongodb.net/parse?retryWrites=true&w=majority');
+    const posts = await res.json();
+    return posts.map((blog) => {
+        return {
+            params: {
+                id: blog.id,
+            },
+        };
+    });
 }
 
 export async function getPostData(id) {
-  // Instead of the file system,
-  // fetch post data from an external API endpoint
-  const res = await fetch(`https://.../posts/${id}`);
-  const post = await res.json();
-  // Combine the data with the id
-  return {
-    id,
-    ...post,
-  };
+    // Instead of the file system,
+    // fetch post data from an external API endpoint
+    const res = await fetch(`https://.../posts/${id}`);
+    const post = await res.json();
+    // Combine the data with the id
+    return {
+        id,
+        ...post,
+    };
 }
